911b1ffe2f6afc6d8afccda78298041899f3e4e1
Shade4827 <shade4827@gmail.com>
2026-09-21 14:14:34 +0900

    ヘッダーを追加し、アプリケーション全体の幅を調整

--- src/App.vue
+++ src/App.vue
@@ -116,99 +116,112 @@ const toggleExcerciseRight = () => {
 const bgColorIsExcerciseRight = computed(() => {
   return isExcerciseRight.value ? 'bg-green-600' : 'bg-gray-500'
 })
+
+const resetAll = () => {
+  resetLuhrgyfCount()
+  resetManaCount()
+  resetEmblemList()
+  spellCount.value = 0
+  isExcerciseRight.value = false
+}
 </script>
 
 <template>
-  <h1>TCG Assist</h1>
+  <header class="flex bg-blue-500 p-1">
+    <h1 class="flex-1 p-1 text-xl text-white font-bold">TCG Assist</h1>
+    <button class="justify-end mr-2 text-white text-2xl" @click="resetAll">↻</button>
+  </header>
 
-  <div class="inline-flex flex-col gap-4 w-92">
-    <ModalButton modal="lhurgoyf" header-text="ルアゴイフカウンター" @click="handleClick">
-      <p>
-        カードタイプ: <span class="font-bold">{{ checkedIds.length }}</span> 種類
-      </p>
-      <div class="flex gap-4">
+  <main class="p-4">
+    <div class="flex flex-col gap-4 w-full">
+      <ModalButton modal="lhurgoyf" header-text="ルアゴイフカウンター" @click="handleClick">
         <p>
-          すべての墓地:
-          <span class="font-bold">{{ allGraveyardCreatureCount }}</span>
+          カードタイプ: <span class="font-bold">{{ checkedIds.length }}</span> 種類
         </p>
+        <div class="flex gap-4">
+          <p>
+            すべての墓地:
+            <span class="font-bold">{{ allGraveyardCreatureCount }}</span>
+          </p>
+          <p>
+            自分の墓地: <span class="font-bold">{{ myGraveyardCreatureCount }}</span>
+          </p>
+        </div>
+      </ModalButton>
+
+      <ModalButton modal="mana" header-text="マナカウンター" @click="handleClick">
+        <div class="flex gap-4">
+          <p>
+            白: <span class="font-bold">{{ manaCount.white.value }}</span>
+          </p>
+          <p>
+            青: <span class="font-bold">{{ manaCount.blue.value }}</span>
+          </p>
+          <p>
+            黒: <span class="font-bold">{{ manaCount.black.value }}</span>
+          </p>
+          <p>
+            赤: <span class="font-bold">{{ manaCount.red.value }}</span>
+          </p>
+          <p>
+            緑: <span class="font-bold">{{ manaCount.green.value }}</span>
+          </p>
+          <p>
+            無: <span class="font-bold">{{ manaCount.colorless.value }}</span>
+          </p>
+        </div>
+      </ModalButton>
+
+      <ModalButton modal="emblem" header-text="紋章管理" @click="handleClick">
         <p>
-          自分の墓地: <span class="font-bold">{{ myGraveyardCreatureCount }}</span>
+          紋章の数: <span class="font-bold">{{ emblemList.length }}</span>
         </p>
+        <p class="w-80 truncate">
+          {{ emblemText }}
+        </p>
+      </ModalButton>
+
+      <hr class="text-gray-400" />
+
+      <div class="flex items-center border rounded p-2 h-12">
+        <p class="flex-1">呪文を唱えた回数:</p>
+        <NumberControl v-model="spellCount" class="h-8 w-32 justify-end" />
       </div>
-    </ModalButton>
 
-    <ModalButton modal="mana" header-text="マナカウンター" @click="handleClick">
-      <div class="flex gap-4">
-        <p>
-          白: <span class="font-bold">{{ manaCount.white.value }}</span>
-        </p>
-        <p>
-          青: <span class="font-bold">{{ manaCount.blue.value }}</span>
-        </p>
-        <p>
-          黒: <span class="font-bold">{{ manaCount.black.value }}</span>
-        </p>
-        <p>
-          赤: <span class="font-bold">{{ manaCount.red.value }}</span>
-        </p>
-        <p>
-          緑: <span class="font-bold">{{ manaCount.green.value }}</span>
-        </p>
-        <p>
-          無: <span class="font-bold">{{ manaCount.colorless.value }}</span>
-        </p>
-      </div>
-    </ModalButton>
-
-    <ModalButton modal="emblem" header-text="紋章管理" @click="handleClick">
-      <p>
-        紋章の数: <span class="font-bold">{{ emblemList.length }}</span>
-      </p>
-      <p class="w-80 truncate">
-        {{ emblemText }}
-      </p>
-    </ModalButton>
-
-    <hr class="text-gray-400" />
-
-    <div class="flex items-center border rounded p-2 h-12">
-      <p class="flex-1">呪文を唱えた回数:</p>
-      <NumberControl v-model="spellCount" class="h-8 w-32 justify-end" />
+      <button
+        type="button"
+        class="border py-2 px-4 rounded text-white font-bold transition duration-300 h-12"
+        :class="bgColorIsExcerciseRight"
+        @click="toggleExcerciseRight"
+      >
+        権利行使: <span class="font-bold">{{ isExcerciseRight ? '✓' : '⚪︎' }}</span>
+      </button>
     </div>
 
-    <button
-      type="button"
-      class="border py-2 px-4 rounded text-white font-bold transition duration-300 h-12"
-      :class="bgColorIsExcerciseRight"
-      @click="toggleExcerciseRight"
-    >
-      権利行使: <span class="font-bold">{{ isExcerciseRight ? '✓' : '⚪︎' }}</span>
-    </button>
-  </div>
+    <LuhrgoyfModal
+      v-model:checkIds="checkedIds"
+      v-model:allGraveyardCreatureCount="allGraveyardCreatureCount"
+      v-model:myGraveyardCreatureCount="myGraveyardCreatureCount"
+      :show="activeModal === 'lhurgoyf'"
+      :items="LURGOYD_CHECKBOXES"
+      @close="closeModal"
+      @reset="resetLuhrgyfCount"
+    />
 
-  <LuhrgoyfModal
-    v-model:checkIds="checkedIds"
-    v-model:allGraveyardCreatureCount="allGraveyardCreatureCount"
-    v-model:myGraveyardCreatureCount="myGraveyardCreatureCount"
-    :show="activeModal === 'lhurgoyf'"
-    :items="LURGOYD_CHECKBOXES"
-    @close="closeModal"
-    @reset="resetLuhrgyfCount"
-  />
+    <ManaModal
+      v-model:manaCount="manaCount"
+      :show="activeModal === 'mana'"
+      @close="closeModal"
+      @reset="resetManaCount"
+    />
 
-  <ManaModal
-    v-model:manaCount="manaCount"
-    :show="activeModal === 'mana'"
-    @close="closeModal"
-    @reset="resetManaCount"
-  />
-
-  <EmblemModal
-    v-model="emblemList"
-    :show="activeModal === 'emblem'"
-    @close="closeModal"
-    @reset="resetEmblemList"
-  />
+    <EmblemModal
+      v-model="emblemList"
+      :show="activeModal === 'emblem'"
+      @close="closeModal"
+      @reset="resetEmblemList"
+    />
+  </main>
 </template>
 
 <style scoped></style>
